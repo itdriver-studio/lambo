862e54f84b57ff0cb32284eae13424234f446a34
Dmitrii <itdriver@yahoo.com>
2024-01-07 20:33:06 +0300

    aadaptive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 const swiper = new Swiper('.swiper', {
     loop: true,
-    slidesPerView: 2,
-    spaceBetween: 20,
+
     navigation: {
         nextEl: '.swiper-button-right',
         prevEl: '.swiper-button-left',
     },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        }
+    }
 });
 
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+    menuMobile.classList.toggle('menu--open');
+});
